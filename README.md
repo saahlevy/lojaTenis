7d5026f355a47b2845c189c06903e7ab61ac7d32
saahlevy <saahlevy@gmail.com>
2024-09-17 16:27:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# lojaTenis" 
+# lojaTenis
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lojaTenis',
+  webDir: 'www'
+};
+
+export default config;
